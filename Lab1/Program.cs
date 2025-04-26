f220e2e21a78015cd0c243186174ef260b39b7a9
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-04-26 16:24:34 +0200

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj Nazwisko");
+        string nazwisko = Console.ReadLine();
+
+        Console.WriteLine("Podaj Imie");
+        string imie = Console.ReadLine();
+
+        Console.WriteLine("Podaj Wiek");
+        string wiekInput = Console.ReadLine();
+        int wiek;
+
+        if (int.TryParse(wiekInput, out wiek))
+        {
+            Console.WriteLine("\nWprowadzone dane:");
+            Console.WriteLine($"Nazwisko: {nazwisko}");
+            Console.WriteLine($"Imię: {imie}");
+            Console.WriteLine($"Wiek: {wiek}");
+        }
+        else
+        {
+            Console.WriteLine("Podano nieprawidłowy wiek!");
+        }
+    }
+}
