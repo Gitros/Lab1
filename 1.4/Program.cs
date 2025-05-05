97e28361ef90ea32ae8d365e0c0c112aa77dfaa0
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-05-05 18:21:39 +0200

    1.4
    Napisz program, który sprawdza, czy podana liczba naturalna jest nieparzysta.

---
+++ 1.4/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę naturalną");
+        int liczbaNaturalna = int.Parse(Console.ReadLine());
+
+        if(liczbaNaturalna % 2 == 0)
+        {
+            Console.WriteLine($"Liczba {liczbaNaturalna} jest parzysta.");
+        } else
+        {
+            Console.WriteLine($"Liczba {liczbaNaturalna} jest nieparzysta.");
+        }
+    }
+}
