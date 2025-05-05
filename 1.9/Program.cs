52385bf9b0890dbb95acd380f10d7d122165dfc1
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-05-05 18:52:47 +0200

    1.9
    Napiszmy program, który 100 elementową tablicę uzupełnia losowymi liczbami naturalnymi, a następnie wyświetla jej zawartość oraz najmniejszą i największą wartość.

---
+++ 1.9/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int[] tablica = new int[100];
+
+        Random rnd = new Random();
+
+        for(int i = 0; i < tablica.Length; i++)
+        {
+            tablica[i] = rnd.Next(1, 101);
+        }
+
+        Console.WriteLine("tablica");
+        foreach(int liczba in tablica)
+        {
+            Console.WriteLine(liczba + " ");
+        }
+        Console.WriteLine();
+
+        int najmniejsza = tablica[0];
+        int najwieksza = tablica[0];
+
+        foreach(int liczba in tablica)
+        {
+            if (liczba < najmniejsza)
+                najmniejsza = liczba;
+            if (liczba > najwieksza)
+                najwieksza = liczba;
+        }
+
+        Console.WriteLine($"Najmniejsza wartość to {najmniejsza}");
+        Console.WriteLine($"Najwieksza wartość to {najwieksza}");
+    }
+}
