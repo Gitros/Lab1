26c451fc32eefaf1eacc0fdbebad2426e98bfe0b
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-05-05 18:16:19 +0200

    Zadanie 1.3
    Napisz program, który zapyta użytkownika o temperaturę ciała i na podstawie podanej liczby wyświetli komunikat:
    
    • „OK”, jeśli wprowadzona liczba jest z zakresu 36,6 do 37,
    
    • „stan podgorączkowy”, jeśli wprowadzona liczba jest z zakresu 37,1 do 38,
    
    • „gorączka”, jeśli wprowadzona liczba jest z zakresu 38,1 do 41
    
    • „skonsultuj się z lekarzem”, dla pozostałych przypadków.

---
+++ 1.3/Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj temperature ciała");
+        decimal temperaturaCiała = Convert.ToDecimal(Console.ReadLine());
+
+        if (temperaturaCiała >= 36.6m && temperaturaCiała <= 37)
+        {
+            Console.WriteLine("OK");
+        }
+        else if (temperaturaCiała >= 37.1m && temperaturaCiała <= 38)
+        {
+            Console.WriteLine("stan podgorączkowy");
+        }
+        else if (temperaturaCiała >= 38.1m && temperaturaCiała <= 41)
+        {
+            Console.WriteLine("gorączka");
+        }
+        else
+        {
+            Console.WriteLine("skonsultuj się z lekarzem");
+        }
+    }
+}
