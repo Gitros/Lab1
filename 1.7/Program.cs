95a59ad6ac60fb03742346bcfd680d01b5519fc7
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-05-05 18:41:43 +0200

    1.7
    Napisz program, który na wejściu otrzyma:
    
    • n – liczba naturalna w systemie dziesiętnym
    
    • s – podstawę docelowego systemu (s<=10).
    
    Na wyjściu wyświetli zapis liczby n w systemie o podstawie s = 2,...,10.

---
+++ 1.7/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę naturalną");
+        int n = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Podaj podstawę systemu");
+        int s = Convert.ToInt32(Console.ReadLine());
+
+        if(s < 2 || s > 10)
+        {
+            Console.WriteLine("Podstawa systemu musi być od 2 do 10");
+            return;
+        }
+
+        string wynik = ConvertToBase(n, s);
+
+        Console.WriteLine($"Liczba {n} w systemie o podstawie {s} to: {wynik}");
+    }
+
+    static string ConvertToBase( int n, int s)
+    {
+        if (n == 0) return "0";
+
+        string result = string.Empty;
+
+        while (n > 0)
+        {
+            result = (n % s) + result;
+            n /= s;
+        }
+
+        return result;
+    }
+}
