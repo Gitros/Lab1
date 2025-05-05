b690b611f7c0ca6c03a2e737aad4986548f20782
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-05-05 18:46:56 +0200

    1.8
    Napisać program, który pobiera od użytkownika liczbę całkowitą, a następnie wyświetli sumę cyfr tej liczby.

---
+++ 1.8/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę naturalną");
+        int liczba = int.Parse(Console.ReadLine());
+
+        int sumaCyfr = SumaCyfr(liczba);
+
+        Console.WriteLine($"Suma cyfr liczby {liczba} wynosi {sumaCyfr}");
+    }
+
+    static int SumaCyfr(int liczba)
+    {
+        liczba = Math.Abs(liczba);
+
+        int suma = 0;
+
+        while (liczba > 0)
+        {
+            suma += liczba % 10;
+            liczba /= 10;
+        }
+
+        return suma;
+    }
+}
