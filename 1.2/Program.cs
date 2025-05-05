54892d3cbb74d39b5b15ca5ee16c37aae2718c03
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-05-05 18:05:49 +0200

    Zadanie 1.2
    Napisz program, który od podanej ceny netto i stawki VAT oblicza kwotę VAT i cenę brutto. Np. cena netto 100 zł, stawka 22% daje kwotę VAT 22 zł i cenę brutto 122 zł. Wyświetlaj kwoty z dwoma miejscami po przecinku, wyświetlając dodawaj symbol waluty i znak % (procent).

--- 1.2/Program.cs
+++ 1.2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj cenę netto");
+        decimal cenaNetto = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Podaj stawkę VAT %");
+        decimal stawkaVAT = Convert.ToDecimal(Console.ReadLine());
+
+        decimal kwotaVAT = cenaNetto * (stawkaVAT / 100);
+
+        decimal cenaBrutto = cenaNetto + kwotaVAT;
+
+        Console.WriteLine($"Kwota VAT: {kwotaVAT:C2}");
+        Console.WriteLine($"Cena brutto: {cenaBrutto:C2}");
+    }
+}
