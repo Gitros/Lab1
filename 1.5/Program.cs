b8b1304ef87ddea690303a7b74c67b80c0d2278a
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-05-05 18:25:31 +0200

    1.5
    Napisz program, który wczytuje liczbę od 1 do 7 i wyświetla nazwę dnia tygodnia (1 to poniedziałek). Zastosuj instrukcję switch.

---
+++ 1.5/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj liczbę od 1 do 7");
+        int dzien = int.Parse(Console.ReadLine());
+
+        switch(dzien)
+        {
+            case 1:
+                Console.WriteLine("Poniedziałek");
+                break;
+            case 2:
+                Console.WriteLine("Wtorek");
+                break;
+            case 3:
+                Console.WriteLine("Środa");
+                break;
+            case 4:
+                Console.WriteLine("Czwartek");
+                break;
+            case 5:
+                Console.WriteLine("Piątek");
+                break;
+            case 6:
+                Console.WriteLine("Sobota");
+                break;
+            case 7:
+                Console.WriteLine("Niedziela");
+                break;
+        }
+    }
+}
