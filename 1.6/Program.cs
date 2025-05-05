7363bf3c4fc7003023d06e0f88d64c29bcbb61a9
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-05-05 18:34:44 +0200

    1.6
    Napisać program, który wczytuje liczby podawane przez użytkownika dotąd, dopóki nie podana zostanie liczba 0. Następnie wyświetlić sumę wszystkich podanych liczb.

---
+++ 1.6/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    static void Main()
+    {
+        int liczba;
+        int suma = 0;
+
+        do
+        {
+            Console.WriteLine("Podaj liczby do sumowania, zakończ podając liczbę 0");
+            liczba = Convert.ToInt32(Console.ReadLine());
+
+            suma += liczba;
+        } while (liczba != 0);
+
+        Console.WriteLine(suma);
+    }
+}
