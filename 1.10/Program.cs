1dfcd9edff48778a467aa9c0eb7df01a571cd861
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2025-05-05 18:58:53 +0200

    1.10
    Napisz program wczytujący do tablicy 10 imion. Na końcu wypisz imiona w odwrotnej kolejności.

---
+++ 1.10/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string[] imiona = new string[10];
+
+        for(int i = 0; i < imiona.Length; i++)
+        {
+            Console.WriteLine($"Podaj imię #{i+1}");
+            imiona[i] = Console.ReadLine();
+        }
+
+        Console.WriteLine("Odwrócone imiona: ");
+        for(int i = imiona.Length - 1; i >= 0; i--)
+        {
+            Console.WriteLine(imiona[i]);
+        }
+    }
+}
